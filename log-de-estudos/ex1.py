d3ff64038f50cf91a1695934bc56e014dff37c4d
Carlos Eduardo <carloseduardofalmada@gmail.com>
2026-06-19 20:31:43 -0300

    Primeira entrega obrigatória trilha back-end Sistemium

---
+++ log-de-estudos/ex1.py
@@ -0,0 +1,7 @@
+entrada = input("Digite um número inteiro:")
+numero = int(entrada)
+
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
---
+++ log-de-estudos/ex3.py
@@ -0,0 +1,4 @@
+aura = int(input("Digite um numero:"))
+
+for aura in range(aura, -1, -1):
+    print(aura)
